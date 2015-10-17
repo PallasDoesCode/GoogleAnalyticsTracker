3983be21941d68fcb99e1bd8efad77b5b9a95a82
RandomlyKnighted <thughes3@jsu.edu>
2015-10-17 15:11:53 -0500

    Added additional tracking methods

--- GoogleAnalyticsTracker/Tracker.cs
+++ GoogleAnalyticsTracker/Tracker.cs
@@ -38,15 +38,108 @@ namespace GoogleAnalyticsTracker
 
         public void TrackPageview(string hostname, string page, string title)
         {
-            if (string.IsNullOrEmpty(hostname)) throw new ArgumentNullException("hostname");
-            if (string.IsNullOrEmpty(page)) throw new ArgumentNullException("action");
-
             var values = DefaultValues;
 
             values.Add("t", HitType.@pageview.ToString());          // Pageview hit type
-            values.Add("dh", hostname);                             // Document hostname. Required.
-            values.Add("dp", page);                                 // Page. Required.
-            if (title != null) values.Add("dt", title);             // Title. Required.
+            values.Add("dh", hostname);                             // Document hostname.
+            values.Add("dp", page);                                 // Page.
+            if (title != null) values.Add("dt", title);             // Title.
+
+            Track(values);
+        }
+
+        public void TrackTransactions(string id, string affiliation, string revenue, string shipping, string tax, string code)
+        {
+            if (string.IsNullOrEmpty(id)) throw new ArgumentNullException("id");
+
+            var values = DefaultValues;
+
+            values.Add("t", HitType.@transaction.ToString());       // Transaction hit type
+            values.Add("ti", id);                                   // Transaction ID. Required.
+            values.Add("ta", affiliation);                          // Transaction affiliation.
+            values.Add("tr", revenue);                              // Transaction revenue.
+            values.Add("ts", shipping);                             // Transaction shipping.
+            values.Add("tt", tax);                                  // Transaction tax.
+            if (code != null) values.Add("cu", code);               // Currency Code.
+
+            Track(values);
+        }
+
+        public void TrackTransactionItem(string id, string name, string price, string quantity, string sku, string variation, string code)
+        {
+            if (string.IsNullOrEmpty(id)) throw new ArgumentNullException("id");
+            if (string.IsNullOrEmpty(name)) throw new ArgumentNullException("name");
+
+            var values = DefaultValues;
+
+            values.Add("t", HitType.@item.ToString());              // Item hit type
+            values.Add("ti", id);                                   // Item ID. Required.
+            values.Add("in", name);                                 // Item name. Required.
+            values.Add("ip", price);                                // Item price.
+            values.Add("iq", quantity);                             // Item quantity.
+            values.Add("ic", sku);                                  // Item code/SKU.
+            values.Add("iv", variation);                            // Item variation/category.
+            if (code != null) values.Add("cu", code);               // Currency Code.
+
+            Track(values);
+        }
+
+        public void TrackSocial(string action, string network, string target)
+        {
+            if (string.IsNullOrEmpty(action)) throw new ArgumentNullException("action");
+            if (string.IsNullOrEmpty(network)) throw new ArgumentNullException("network");
+            if (string.IsNullOrEmpty(target)) throw new ArgumentNullException("target");
+
+            var values = DefaultValues;
+
+            values.Add("t", HitType.@social.ToString());            // Social hit type
+            values.Add("sa", action);                               // Social Action. Required.
+            values.Add("sn", network);                              // Social Network. Required.
+            if (target != null) values.Add("st", target);           // Social Target. Required.
+
+            Track(values);
+        }
+
+        public void TrackException(string description, string isFatal)
+        {
+            var values = DefaultValues;
+
+            values.Add("t", HitType.@exception.ToString());         // Exception hit type
+            values.Add("exd", description);                         // Exception description
+            if (isFatal != null) values.Add("exf", isFatal);        // Exception is fatal?
+
+            Track(values);
+        }
+
+        private void TrackUserTiming()
+        {
+            //var values = DefaultValues;
+
+            //values.Add("t", HitType.@timing.ToString());          // Timing hit type
+            //values.Add("utc", category);                          // Timing category.
+            //values.Add("utv", variable);                          // Timing variable.
+            //values.Add("utt", time);                              // Timing time.
+            //values.Add("utl", label);                             // Timing label.
+
+            //values.Add("dns", category);                          // DNS load time.
+            //values.Add("pdt", variable);                          // Page download time.
+            //values.Add("rrt", time);                              // Redirect time.
+            //values.Add("tcp", label);                             // TCP connect time.
+            //values.Add("srt", label);                             // Server response time.
+
+            //Track(values);
+        }
+
+        public void TrackScreenview(string name, string version, string id, string installerId, string description)
+        {
+            var values = DefaultValues;
+
+            values.Add("t", HitType.@screenview.ToString());        // Screenview hit type
+            values.Add("an", name);                                 // App name.
+            values.Add("av", version);                              // App version.
+            values.Add("aid", id);                                  // App Id.
+            values.Add("aiid", installerId);                        // App Installer Id.
+            values.Add("cd", description);                          // Screen name/content description.
 
             Track(values);
         }
@@ -91,9 +184,14 @@ namespace GoogleAnalyticsTracker
 
         private enum HitType
         {
-            // t is the HitType
-            @event,     // Used for event tracking, measuring actions, combining impressions and actions, measuring refunds
-            @pageview,  // Used for page tracking, measuring impressions, measuring purchases, measuring the checkout process
+            @event,
+            @pageview,
+            @transaction,
+            @item,
+            @social,
+            @exception,
+            @timing,
+            @screenview,
         }
 
         private Dictionary<string, string> DefaultValues
