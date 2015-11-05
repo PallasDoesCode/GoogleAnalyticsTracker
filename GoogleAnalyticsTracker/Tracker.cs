116712ffcc909c5c40acf62c085b64a39ea2241f
RandomlyKnighted <thughes3@jsu.edu>
2015-11-04 21:43:58 -0600

    Improved string comparisons

--- GoogleAnalyticsTracker/Tracker.cs
+++ GoogleAnalyticsTracker/Tracker.cs
@@ -27,11 +27,11 @@ namespace GoogleAnalyticsTracker
 
             var values = DefaultValues;
 
-            values.Add("t", HitType.@event.ToString());             // Event hit type
-            values.Add("ec", category);                             // Event Category. Required.
-            values.Add("ea", action);                               // Event Action. Required.
-            if (label != null) values.Add("el", label);             // Event label.
-            if (value != null) values.Add("ev", value.ToString());  // Event value.
+            values.Add("t", HitType.@event.ToString());                                 // Event hit type
+            values.Add("ec", category);                                                 // Event Category. Required.
+            values.Add("ea", action);                                                   // Event Action. Required.
+            if (string.IsNullOrWhiteSpace(label)) values.Add("el", label);              // Event label.
+            if (value != null) values.Add("ev", value.ToString());                      // Event value.
 
             Track(values);
         }
@@ -40,10 +40,10 @@ namespace GoogleAnalyticsTracker
         {
             var values = DefaultValues;
 
-            values.Add("t", HitType.@pageview.ToString());          // Pageview hit type
-            values.Add("dh", hostname);                             // Document hostname.
-            values.Add("dp", page);                                 // Page.
-            if (title != null) values.Add("dt", title);             // Title.
+            values.Add("t", HitType.@pageview.ToString());                              // Pageview hit type
+            values.Add("dh", hostname);                                                 // Document hostname.
+            values.Add("dp", page);                                                     // Page.
+            if (string.IsNullOrWhiteSpace(title)) values.Add("dt", title);              // Title.
 
             Track(values);
         }
@@ -54,13 +54,13 @@ namespace GoogleAnalyticsTracker
 
             var values = DefaultValues;
 
-            values.Add("t", HitType.@transaction.ToString());       // Transaction hit type
-            values.Add("ti", id);                                   // Transaction ID. Required.
-            values.Add("ta", affiliation);                          // Transaction affiliation.
-            values.Add("tr", revenue);                              // Transaction revenue.
-            values.Add("ts", shipping);                             // Transaction shipping.
-            values.Add("tt", tax);                                  // Transaction tax.
-            if (code != null) values.Add("cu", code);               // Currency Code.
+            values.Add("t", HitType.@transaction.ToString());                           // Transaction hit type
+            values.Add("ti", id);                                                       // Transaction ID. Required.
+            values.Add("ta", affiliation);                                              // Transaction affiliation.
+            values.Add("tr", revenue);                                                  // Transaction revenue.
+            values.Add("ts", shipping);                                                 // Transaction shipping.
+            values.Add("tt", tax);                                                      // Transaction tax.
+            if (string.IsNullOrWhiteSpace(code)) values.Add("cu", code);                // Currency Code.
 
             Track(values);
         }
@@ -72,14 +72,14 @@ namespace GoogleAnalyticsTracker
 
             var values = DefaultValues;
 
-            values.Add("t", HitType.@item.ToString());              // Item hit type
-            values.Add("ti", id);                                   // Item ID. Required.
-            values.Add("in", name);                                 // Item name. Required.
-            values.Add("ip", price);                                // Item price.
-            values.Add("iq", quantity);                             // Item quantity.
-            values.Add("ic", sku);                                  // Item code/SKU.
-            values.Add("iv", variation);                            // Item variation/category.
-            if (code != null) values.Add("cu", code);               // Currency Code.
+            values.Add("t", HitType.@item.ToString());                                  // Item hit type
+            values.Add("ti", id);                                                       // Item ID. Required.
+            values.Add("in", name);                                                     // Item name. Required.
+            values.Add("ip", price);                                                    // Item price.
+            values.Add("iq", quantity);                                                 // Item quantity.
+            values.Add("ic", sku);                                                      // Item code/SKU.
+            values.Add("iv", variation);                                                // Item variation/category.
+            if (string.IsNullOrWhiteSpace(code)) values.Add("cu", code);                // Currency Code.
 
             Track(values);
         }
@@ -92,10 +92,10 @@ namespace GoogleAnalyticsTracker
 
             var values = DefaultValues;
 
-            values.Add("t", HitType.@social.ToString());            // Social hit type
-            values.Add("sa", action);                               // Social Action. Required.
-            values.Add("sn", network);                              // Social Network. Required.
-            if (target != null) values.Add("st", target);           // Social Target. Required.
+            values.Add("t", HitType.@social.ToString());                                // Social hit type
+            values.Add("sa", action);                                                   // Social Action. Required.
+            values.Add("sn", network);                                                  // Social Network. Required.
+            if (string.IsNullOrWhiteSpace(target)) values.Add("st", target);            // Social Target. Required.
 
             Track(values);
         }
@@ -104,9 +104,9 @@ namespace GoogleAnalyticsTracker
         {
             var values = DefaultValues;
 
-            values.Add("t", HitType.@exception.ToString());         // Exception hit type
-            values.Add("exd", description);                         // Exception description
-            if (isFatal != null) values.Add("exf", isFatal);        // Exception is fatal?
+            values.Add("t", HitType.@exception.ToString());                             // Exception hit type
+            values.Add("exd", description);                                             // Exception description
+            if (isFatal != null) values.Add("exf", isFatal);                            // Exception is fatal?
 
             Track(values);
         }
@@ -115,17 +115,17 @@ namespace GoogleAnalyticsTracker
         //{
             //var values = DefaultValues;
 
-            //values.Add("t", HitType.@timing.ToString());          // Timing hit type
-            //values.Add("utc", category);                          // Timing category.
-            //values.Add("utv", variable);                          // Timing variable.
-            //values.Add("utt", time);                              // Timing time.
-            //values.Add("utl", label);                             // Timing label.
+            //values.Add("t", HitType.@timing.ToString());                              // Timing hit type
+            //values.Add("utc", category);                                              // Timing category.
+            //values.Add("utv", variable);                                              // Timing variable.
+            //values.Add("utt", time);                                                  // Timing time.
+            //values.Add("utl", label);                                                 // Timing label.
 
-            //values.Add("dns", category);                          // DNS load time.
-            //values.Add("pdt", variable);                          // Page download time.
-            //values.Add("rrt", time);                              // Redirect time.
-            //values.Add("tcp", label);                             // TCP connect time.
-            //values.Add("srt", label);                             // Server response time.
+            //values.Add("dns", category);                                              // DNS load time.
+            //values.Add("pdt", variable);                                              // Page download time.
+            //values.Add("rrt", time);                                                  // Redirect time.
+            //values.Add("tcp", label);                                                 // TCP connect time.
+            //values.Add("srt", label);                                                 // Server response time.
 
             //Track(values);
         //}
@@ -134,12 +134,12 @@ namespace GoogleAnalyticsTracker
         {
             var values = DefaultValues;
 
-            values.Add("t", HitType.@screenview.ToString());        // Screenview hit type
-            values.Add("an", name);                                 // App name.
-            values.Add("av", version);                              // App version.
-            values.Add("aid", id);                                  // App Id.
-            values.Add("aiid", installerId);                        // App Installer Id.
-            values.Add("cd", description);                          // Screen name/content description.
+            values.Add("t", HitType.@screenview.ToString());                            // Screenview hit type
+            values.Add("an", name);                                                     // App name.
+            values.Add("av", version);                                                  // App version.
+            values.Add("aid", id);                                                      // App Id.
+            values.Add("aiid", installerId);                                            // App Installer Id.
+            values.Add("cd", description);                                              // Screen name/content description.
 
             Track(values);
         }
@@ -200,9 +200,9 @@ namespace GoogleAnalyticsTracker
             {
                 var data = new Dictionary<string, string>()
                 {
-                    { "v", "1" },                   // The protocol version. The value should be 1.
-                    { "tid", googleTrackingID },    // Tracking ID / Web property / Property ID.
-                    { "cid", googleClientID }       // Anonymous Client ID (must be unique).
+                    { "v", "1" },                                                       // The protocol version. The value should be 1.
+                    { "tid", googleTrackingID },                                        // Tracking ID / Web property / Property ID.
+                    { "cid", googleClientID }                                           // Anonymous Client ID (must be unique).
                 };
 
                 return data;
