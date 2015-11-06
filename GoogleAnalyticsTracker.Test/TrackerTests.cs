2d920ba2b20b5da389e888e4404f64ff56051793
RandomlyKnighted <thughes3@jsu.edu>
2015-11-05 18:48:21 -0600

    Changed Unit Test project name

---
+++ GoogleAnalyticsTracker.Test/TrackerTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GoogleAnalyticsTracker.Test
+{
+    [TestClass]
+    public class TrackerTests
+    {
+        [TestMethod]
+        public void TestEventTracker()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestPageviewTracker()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestTransactionTracker()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestTransactionItemTracker()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestSocialTracker()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestExceptionTracker()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestUserTimingTracker()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestScreenviewTracker()
+        {
+
+        }
+    }
+}
