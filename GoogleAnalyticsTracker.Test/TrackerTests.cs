c5b32cd5f0efba1c88f152f1c327957282e3bedb
RandomlyKnighted <thughes3@jsu.edu>
2015-12-24 19:10:04 -0600

    Added first unit test using Moq framework

--- GoogleAnalyticsTracker.Test/TrackerTests.cs
+++ GoogleAnalyticsTracker.Test/TrackerTests.cs
@@ -1,57 +1,68 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.IO;
+using System.Net;
 
 namespace GoogleAnalyticsTracker.Test
 {
     [TestClass]
     public class TrackerTests
     {
-        [TestMethod]
-        public void TestEventTracker()
-        {
+        private readonly string url = "http://www.google-analytics.com/collect";
 
+        private readonly Mock<IWebRequestCreate> _MockWebRequestCreate = new Mock<IWebRequestCreate>();
+        private readonly Mock<HttpWebRequest> _MockWebRequest = new Mock<HttpWebRequest>();
+        private readonly Mock<Stream> _MockRequestStream = new Mock<Stream>();
+        private readonly Mock<HttpWebResponse> _MockResponse = new Mock<HttpWebResponse>();
+
+        /// <summary>
+        /// Sets up mock objects
+        /// </summary>
+        [TestInitialize]
+        public void Initialize()
+        {
+            this._MockWebRequestCreate
+                .Setup(s => s.Create(It.IsAny<Uri>()))
+                .Returns(this._MockWebRequest.Object);
+
+            this._MockWebRequest
+                .Setup(s => s.GetRequestStream())
+                .Returns(this._MockRequestStream.Object);
+
+            this._MockRequestStream
+                .Setup(s => s.CanWrite)
+                .Returns(true);
+
+            this._MockWebRequest
+              .Setup(s => s.GetResponse())
+              .Returns(this._MockResponse.Object);
+
+            this._MockResponse
+                .Setup(s => s.StatusCode)
+                .Returns(HttpStatusCode.OK);
         }
 
         [TestMethod]
-        public void TestPageviewTracker()
+        public void TestTrackMethod()
         {
+            // Arrange
+            var request = (HttpWebRequest)this._MockWebRequestCreate.Object.Create(new Uri(url));
+            request.Method = WebRequestMethods.Http.Post;
 
-        }
+            // Act
+            var requestStream = request.GetRequestStream();
 
-        [TestMethod]
-        public void TestTransactionTracker()
-        {
+            using (var writer = new StreamWriter(requestStream))
+            {
+                writer.Write("v=1&t=event&tid=UA-70051512-1&cid=729&ec=Category&ea=Action&el=Label");
+            }
 
-        }
-
-        [TestMethod]
-        public void TestTransactionItemTracker()
-        {
-
-        }
-
-        [TestMethod]
-        public void TestSocialTracker()
-        {
-
-        }
-
-        [TestMethod]
-        public void TestExceptionTracker()
-        {
-
-        }
-
-        [TestMethod]
-        public void TestUserTimingTracker()
-        {
-
-        }
-
-        [TestMethod]
-        public void TestScreenviewTracker()
-        {
-
+            // Assert
+            using (var response = (HttpWebResponse)request.GetResponse())
+            {
+                Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            }
         }
     }
 }
